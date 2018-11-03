42e0e6bda6fe568f34fd550cf9bbd0dc9a3013d9
b-akram <ga96me@gmail.com>
2018-11-03 17:40:56 +0100

    portregle

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require("express")
 let app = express()
 let path = require("path")
 app.use("/",express.static(path.join(__dirname,"static")))
+const port=process.env.PORT || 3000
 
-app.listen(80)
+app.listen(port)
  
