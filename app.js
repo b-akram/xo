04d9b470e987ad7278f088222a1c265aace7ae8a
b-akram <ga96me@gmail.com>
2018-11-04 00:49:08 +0100

    #reformat

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ let path = require("path")
 app.use("/",express.static(path.join(__dirname,"static")))
 const port=process.env.PORT || 3000
 app.get("/aj",function (req,res) {
-    port=process.env.PORT
-    res.send("port : "+port)
+    let port1=process.env.PORT
+    res.send("port : "+port1)
 })
 app.listen(port)
  
